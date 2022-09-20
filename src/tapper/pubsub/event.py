7e93517fc980c9471992d04e68d6dc493e7b1b54
Illia Halatiuk <galatyuk.ilya@gmail.com>
2022-09-20 21:20:35 +0200

    pubsub

---
+++ src/tapper/pubsub/event.py
@@ -0,0 +1,42 @@
+""" Publisher-subscriber implementation.
+
+    Typical usage example:
+
+    In subscriber:
+        event.subscribe(PREDEFINED_TOPIC_NAME, function_that_will_receive_messages)
+
+    In publisher:
+        event.publish(PREDEFINED_TOPIC_NAME, message_of_any_type)
+"""
+from typing import Any
+from typing import Callable
+
+SubscribedFunction = Callable[[Any], None]
+
+_subscribers: dict[str, list[SubscribedFunction]] = dict()
+
+
+def subscribe(topic: str, subscribed_function: SubscribedFunction) -> None:
+    """Subscribes a function to messages from a topic
+
+    :param topic: predefined string
+    :param subscribed_function: function that will receive messages.
+        It has to accept one parameter, and should be compatible
+        with publisher's message' data type
+    """
+    if topic not in _subscribers:
+        _subscribers[topic] = []
+    _subscribers[topic].append(subscribed_function)
+
+
+def publish(topic: str, message: Any) -> None:
+    """Publishes any data as message
+
+    :param topic: predefined string
+    :param message: Any data type, dataclass of this
+        should in a separate module to decouple from subscribers
+    """
+    if topic not in _subscribers:
+        return
+    for subscribed_function in _subscribers[topic]:
+        subscribed_function(message)
---
+++ tests/tapper/pubsub/test_event.py
@@ -0,0 +1,22 @@
+from typing import Any
+
+import hypothesis
+from hypothesis import strategies as st
+from tapper.pubsub import event
+
+
+class Subscriber:
+    received_message: Any
+
+    def receive_message(self, sub_message: Any) -> None:
+        self.received_message = sub_message
+
+
+@hypothesis.given(st.integers() | st.text() | st.datetimes())
+@hypothesis.settings(max_examples=20)
+def test_pubsub(message: Any) -> None:
+    subscriber = Subscriber()
+    event.subscribe("topic_name", subscriber.receive_message)
+    event.publish("topic_name", message)
+
+    assert subscriber.received_message == message
