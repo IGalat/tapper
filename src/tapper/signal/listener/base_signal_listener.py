aacc8a4b31597ada8b309e411e70cbb15376410e
Illia Halatiuk <galatyuk.ilya@gmail.com>
2022-09-28 10:42:24 +0200

    doc

--- src/tapper/signal/listener/base_signal_listener.py
+++ src/tapper/signal/listener/base_signal_listener.py
@@ -5,7 +5,7 @@ from abc import abstractmethod
 class SignalListener(ABC):
     """Listens to signals that may trigger actions.
 
-    Will run in a separate thread.
+    When an implementation receives the signal, it must call on_signal fn.
     """
 
     @abstractmethod
@@ -17,14 +17,20 @@ class SignalListener(ABC):
 
         Do not override.
 
-        :param symbol: Must be symbol from get_possible_signal_symbols
-        :param down:
-        :return:
+        :param symbol: Must be a symbol from get_possible_signal_symbols.
+        :param down: Is key pressed down? False if it is released.
+
+        :return: If True: signal must be propagated further to system as normal.
+                 If False: signal must be suppressed, so that other apps don't
+                    receive it. This means an action has been triggered.
         """
 
     @abstractmethod
     def start(self) -> None:
-        """Initialize resources and start listening."""
+        """Initialize resources and start listening.
+
+        This will be launched in a separate thread.
+        """
 
     @abstractmethod
     def stop(self) -> None:
