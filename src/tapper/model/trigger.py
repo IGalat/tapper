6ad1a40b2dfa9ae3850bb51b7e8a4684b0db1708
Illia Halatiuk <galatyuk.ilya@gmail.com>
2022-10-07 00:12:32 +0200

    modify model

--- src/tapper/model/trigger.py
+++ src/tapper/model/trigger.py
@@ -1,8 +1,13 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
+from dataclasses import field
+
+from tapper.util import type_check
 
 
-@dataclass(frozen=True)
-class _TriggerKey:
+@dataclass(init=False)
+class TriggerKey:
     """A single key in trigger."""
 
     symbols: list[str]
@@ -13,20 +18,32 @@ class _TriggerKey:
             For 'lmb' it's ['left_mouse_button']
             For 'alt' it's ['right_alt', 'left_alt', 'virtual_alt]
     """
-    time: float
-    """For main key: minimum time it has to be pressed for trigger to work.
-    For auxiliary keys: minimum time between its press and main key press for the trigger to work."""
+    time: float = field(default=0)
+    """For main key: minimum time it has to be pressed for trigger to work. (! or released after. see future plans)
+    For auxiliary keys: minimum time between its press and main signal for the trigger to work."""
+
+    def __init__(self, symbols: list[str], time: float = 0) -> None:
+        self.symbols = symbols
+        self.time = time
 
 
 @dataclass(frozen=True)
 class Trigger:
     """Signals are compared against this to determine if action should be performed."""
 
-    auxiliary: list[_TriggerKey]
-    """List of keys that have to be pressed before main key for trigger to work."""
-
-    main: _TriggerKey
+    main: TriggerKey
     """Main key that actually triggers the action, if other conditions are met."""
 
-    main_direction: bool
+    main_direction: bool = True
     """When to trigger on main key: down(press)(True)/up(release)(False)."""
+
+    auxiliary: list[TriggerKey] = field(default_factory=list)
+    """List of keys that have to be pressed before main key for trigger to work."""
+
+    @classmethod
+    def from_simple(cls, main: list[str], aux: list[str] | None = None) -> Trigger:
+        main_ = TriggerKey(main)
+        aux_: list[TriggerKey] = []
+        if type_check.is_list_of(aux, str):
+            aux_ = [TriggerKey(aux)]  # type: ignore
+        return Trigger(main=main_, auxiliary=aux_)
