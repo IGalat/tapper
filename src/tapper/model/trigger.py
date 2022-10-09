6ef7e3427c9154c801950e9ae7094c9040ea8598
Illia Halatiuk <galatyuk.ilya@gmail.com>
2022-10-09 13:59:42 +0200

    change trigger model and fix test accordingly

--- src/tapper/model/trigger.py
+++ src/tapper/model/trigger.py
@@ -1,13 +1,12 @@
 from __future__ import annotations
 
+from abc import ABC
 from dataclasses import dataclass
 from dataclasses import field
 
-from tapper.util import type_check
 
-
-@dataclass(init=False)
-class TriggerKey:
+@dataclass
+class TriggerKey(ABC):
     """A single key in trigger."""
 
     symbols: list[str]
@@ -16,34 +15,32 @@ class TriggerKey:
         Example:
             For 'a' it's just ['a']
             For 'lmb' it's ['left_mouse_button']
-            For 'alt' it's ['right_alt', 'left_alt', 'virtual_alt]
+            For 'alt' it's ['right_alt', 'left_alt', 'virtual_alt']
     """
-    time: float = field(default=0)
-    """For main key: minimum time it has to be pressed for trigger to work. (! or released after. see future plans)
-    For auxiliary keys: minimum time between its press and main signal for the trigger to work."""
 
-    def __init__(self, symbols: list[str], time: float = 0) -> None:
-        self.symbols = symbols
-        self.time = time
+
+@dataclass
+class MainKey(TriggerKey):
+    """Key that actually triggers the action, if other conditions are met."""
+
+    time: float = field(default=0)
+    """Minimum time it has to be pressed for trigger to work. (! or released after. see future plans)"""
+    direction: bool = True
+    """When to trigger on main key: down(press)(True)/up(release)(False)."""
+
+
+@dataclass
+class AuxiliaryKey(TriggerKey):
+    """Key that must be pressed in a combination with main key."""
+
+    time: float = field(default=0)
+    """Minimum time between its press and main signal for the trigger to work."""
 
 
 @dataclass(frozen=True)
 class Trigger:
     """Signals are compared against this to determine if action should be performed."""
 
-    main: TriggerKey
-    """Main key that actually triggers the action, if other conditions are met."""
-
-    main_direction: bool = True
-    """When to trigger on main key: down(press)(True)/up(release)(False)."""
-
-    auxiliary: list[TriggerKey] = field(default_factory=list)
+    main: MainKey
+    aux: list[AuxiliaryKey] = field(default_factory=list)
     """List of keys that have to be pressed before main key for trigger to work."""
-
-    @classmethod
-    def from_simple(cls, main: list[str], aux: list[str] | None = None) -> Trigger:
-        main_ = TriggerKey(main)
-        aux_: list[TriggerKey] = []
-        if type_check.is_list_of(aux, str):
-            aux_ = [TriggerKey(aux)]  # type: ignore
-        return Trigger(main=main_, auxiliary=aux_)
--- tests/tapper/test_parser.py
+++ tests/tapper/test_parser.py
@@ -6,8 +6,9 @@ from tapper.model import constants
 from tapper.model import keyboard
 from tapper.model import mouse
 from tapper.model.errors import TriggerParseError
+from tapper.model.trigger import AuxiliaryKey
+from tapper.model.trigger import MainKey
 from tapper.model.trigger import Trigger
-from tapper.model.trigger import TriggerKey
 
 shift_list = keyboard.aliases["shift"]
 alt_list = keyboard.aliases["alt"]
@@ -27,18 +28,18 @@ class TestTriggerParser:
             parser.TriggerParser([keyboard.get_keys(), {"a": ["a"]}])
 
     def test_one_symbol_simplest(self, parse: ParseFn) -> None:
-        assert parse("a") == Trigger.from_simple(main=["a"])
+        assert parse("a") == Trigger(main=MainKey(["a"]))
 
     def test_non_registered_symbol(self, parse: ParseFn) -> None:
         with pytest.raises(TriggerParseError):
             parse("qwerasdf")
 
     def test_with_alias(self, parse: ParseFn) -> None:
-        assert parse("mmb") == Trigger.from_simple(main=["middle_mouse_button"])
+        assert parse("mmb") == Trigger(main=MainKey(["middle_mouse_button"]))
 
     def test_combo_with_aliases(self, parse: ParseFn) -> None:
-        assert parse("esc+scroll_up") == Trigger.from_simple(
-            main=["escape"], aux=["scroll_wheel_up"]
+        assert parse("esc+scroll_up") == Trigger(
+            main=MainKey(["scroll_wheel_up"]), aux=[AuxiliaryKey(["escape"])]
         )
 
     def test_combo_with_nothing(self, parse: ParseFn) -> None:
@@ -48,49 +49,55 @@ class TestTriggerParser:
             parse("+home")
 
     def test_alias_points_to_many(self, parse: ParseFn) -> None:
-        assert parse("control") == Trigger.from_simple(
-            main=["left_control", "right_control", "virtual_control"]
-        )
+        assert parse("control") == Trigger(main=MainKey(ctrl_list))
 
     def test_alias_with_modifier(self, parse: ParseFn) -> None:
-        assert parse("alt+f1") == Trigger.from_simple(main=["f1"], aux=alt_list)
+        assert parse("alt+f1") == Trigger(
+            main=MainKey(["f1"]), aux=[AuxiliaryKey(alt_list)]
+        )
 
     def test_uppercase(self, parse: ParseFn) -> None:
-        assert parse("R") == Trigger.from_simple(main=["r"], aux=shift_list)
+        assert parse("R") == Trigger(
+            main=MainKey(["r"]), aux=[AuxiliaryKey(shift_list)]
+        )
 
     def test_many_modifiers(self, parse: ParseFn) -> None:
         assert parse("ctrl+lalt+right_shift+scroll_down") == Trigger(
-            main=TriggerKey(["scroll_wheel_down"]),
-            auxiliary=[
-                TriggerKey(ctrl_list),
-                TriggerKey(["left_alt"]),
-                TriggerKey(["right_shift"]),
+            main=MainKey(["scroll_wheel_down"]),
+            aux=[
+                AuxiliaryKey(ctrl_list),
+                AuxiliaryKey(["left_alt"]),
+                AuxiliaryKey(["right_shift"]),
             ],
         )
 
     def test_shift_and_uppercase(self, parse: ParseFn) -> None:
-        assert parse("shift+A") == Trigger.from_simple(main=["a"], aux=shift_list)
+        assert parse("shift+A") == Trigger(
+            main=MainKey(["a"]), aux=[AuxiliaryKey(shift_list)]
+        )
         assert parse("shift+A") == parse("A")
 
     def test_one_aux_many_main(self, parse: ParseFn) -> None:
-        assert parse("clear+ctrl") == Trigger.from_simple(
-            main=shift_list, aux=["clear"]
+        assert parse("clear+ctrl") == Trigger(
+            main=MainKey(shift_list), aux=[AuxiliaryKey(["clear"])]
         )
 
     def test_control_symbols(self, parse: ParseFn) -> None:
-        assert parse("\t+\n") == Trigger.from_simple(main=["enter"], aux=["tab"])
+        assert parse("\t+\n") == Trigger(
+            main=MainKey(["enter"]), aux=[AuxiliaryKey(["tab"])]
+        )
 
     def test_space(self, parse: ParseFn) -> None:
-        assert parse(" ") == Trigger.from_simple(main=["space"])
+        assert parse(" ") == Trigger(main=MainKey(["space"]))
 
     def test_plus(self, parse: ParseFn) -> None:
         assert parse("1++") == Trigger(
-            main=TriggerKey(["="]),
-            auxiliary=[TriggerKey(["1"]), TriggerKey(shift_list)],
+            main=MainKey(["="]),
+            aux=[AuxiliaryKey(["1"]), AuxiliaryKey(shift_list)],
         )
         assert parse("++-") == Trigger(
-            main=TriggerKey(["-"]),
-            auxiliary=[TriggerKey(["="]), TriggerKey(shift_list)],
+            main=MainKey(["-"]),
+            aux=[AuxiliaryKey(["="]), AuxiliaryKey(shift_list)],
         )
 
     def test_same_symbol(self, parse: ParseFn) -> None:
@@ -103,70 +110,68 @@ class TestTriggerParser:
 
     def test_two_capital_letters(self, parse: ParseFn) -> None:
         assert parse("A+B") == Trigger(
-            main=TriggerKey(["b"]),
-            auxiliary=[TriggerKey(["a"]), TriggerKey(shift_list)],
+            main=MainKey(["b"]),
+            aux=[AuxiliaryKey(["a"]), AuxiliaryKey(shift_list)],
         )
 
     def test_space_and_plus(self, parse: ParseFn) -> None:
         assert parse("++ ") == Trigger(
-            main=TriggerKey(["space"]),
-            auxiliary=[TriggerKey(["="]), TriggerKey(shift_list)],
+            main=MainKey(["space"]),
+            aux=[AuxiliaryKey(["="]), AuxiliaryKey(shift_list)],
         )
         assert parse(" ++") == Trigger(
-            main=TriggerKey(["="]),
-            auxiliary=[TriggerKey(["space"]), TriggerKey(shift_list)],
+            main=MainKey(["="]),
+            aux=[AuxiliaryKey(["space"]), AuxiliaryKey(shift_list)],
         )
 
     def test_up_simplest(self, parse: ParseFn) -> None:
         assert parse("num0 up") == Trigger(
-            main=TriggerKey(["num0"]), main_direction=constants.KEY_DIR.UP
+            main=MainKey(["num0"], direction=constants.KEY_DIR.UP)
         )
 
     def test_up_with_modifier(self, parse: ParseFn) -> None:
         assert parse("\\+] up") == Trigger(
-            main=TriggerKey(["]"]),
-            auxiliary=[TriggerKey(["\\"])],
-            main_direction=constants.KEY_DIR.UP,
+            main=MainKey(["]"], direction=constants.KEY_DIR.UP),
+            aux=[AuxiliaryKey(["\\"])],
         )
 
     def test_time_simplest(self, parse: ParseFn) -> None:
         assert (
             parse("space 0.5s")
             == parse("space 500ms")
-            == Trigger(main=TriggerKey(["space"], time=0.5))
+            == Trigger(main=MainKey(["space"], time=0.5))
         )
 
     def test_time_with_aux(self, parse: ParseFn) -> None:
         assert parse("ctrl+a 1s") == Trigger(
-            main=TriggerKey(["a"], time=1),
-            auxiliary=[TriggerKey(ctrl_list)],
+            main=MainKey(["a"], time=1),
+            aux=[AuxiliaryKey(ctrl_list)],
         )
 
     def test_time_on_aux(self, parse: ParseFn) -> None:
         assert parse("f3 300ms+\n") == Trigger(
-            main=TriggerKey(["enter"]),
-            auxiliary=[TriggerKey(["f3"], time=0.3)],
+            main=MainKey(["enter"]),
+            aux=[AuxiliaryKey(["f3"], time=0.3)],
         )
 
     def test_time_main_and_aux(self, parse: ParseFn) -> None:
         assert parse("num_lock 1s+del 300ms") == Trigger(
-            main=TriggerKey(["delete"], time=0.3),
-            auxiliary=[TriggerKey(["num_lock"], time=1)],
+            main=MainKey(["delete"], time=0.3),
+            aux=[AuxiliaryKey(["num_lock"], time=1)],
         )
 
     def test_up_time(self, parse: ParseFn) -> None:
         assert parse("q 2s up") == Trigger(
-            main=TriggerKey(["q"], time=2), main_direction=constants.KEY_DIR.UP
+            main=MainKey(["q"], time=2, direction=constants.KEY_DIR.UP)
         )
 
     def test_up_time_with_mod_time(self, parse: ParseFn) -> None:
         assert parse("alt 1s+esc 350ms up") == Trigger(
-            main=TriggerKey(["escape"], time=0.35),
-            main_direction=constants.KEY_DIR.UP,
-            auxiliary=[TriggerKey(alt_list, time=1)],
+            main=MainKey(["escape"], time=0.35, direction=constants.KEY_DIR.UP),
+            aux=[AuxiliaryKey(alt_list, time=1)],
         )
 
     def test_up_time_different_order(self, parse: ParseFn) -> None:
         assert parse("g up 1s") == Trigger(
-            main=TriggerKey(["g"], time=1), main_direction=constants.KEY_DIR.UP
+            main=MainKey(["g"], time=1, direction=constants.KEY_DIR.UP)
         )
