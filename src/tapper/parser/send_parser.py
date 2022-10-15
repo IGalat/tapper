eb30a785e7489e2355eaf7fd3a4f04705520bd38
Illia Halatiuk <galatyuk.ilya@gmail.com>
2022-10-15 19:41:56 +0200

    send parser stub

--- src/tapper/parser/send_parser.py
+++ src/tapper/parser/send_parser.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from dataclasses import field
 
 from tapper.model.send import SendInstruction
 
@@ -6,6 +7,11 @@ from tapper.model.send import SendInstruction
 @dataclass
 class SendParser:
     """Parses the 'send' command."""
+
+    symbol_inn_map: dict[str, type[SendInstruction]] = field(default_factory=dict)
+    """Symbol and its corresponding command."""
+    regex_inn_map: dict[str, type[SendInstruction]] = field(default_factory=dict)
+    """Regex and corresponding command."""
 
     def parse(self, command: str) -> list[SendInstruction]:
         """Parse send command into sequential instructions."""
