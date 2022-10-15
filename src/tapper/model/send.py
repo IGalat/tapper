e11c3a2bac5c1a5b818ab2c01e13183d621db5e0
Illia Halatiuk <galatyuk.ilya@gmail.com>
2022-10-15 20:30:01 +0200

    add init from str for key instruction

--- src/tapper/model/send.py
+++ src/tapper/model/send.py
@@ -8,12 +8,16 @@ class SendInstruction(ABC):
     """Single instruction parsed from send command."""
 
 
-@dataclass
+@dataclass(init=False)
 class KeyInstruction(SendInstruction):
     """Regular key, such as on keyboard or mouse button. Generic."""
 
     symbol: str
     dir: constants.KEY_DIR
+
+    def __init__(self, symbol: str, dir: constants.KEY_DIR | str) -> None:
+        self.symbol = symbol
+        self.dir = dir if isinstance(dir, constants.KEY_DIR) else constants.KEY_DIR[dir]
 
 
 @dataclass
