d5dad754060f33c88fa1d7754c8a6b8124a52cd7
Illia Halatiuk <galatyuk.ilya@gmail.com>
2022-12-21 00:28:14 -0500

    docs for actions helper

--- src/tapper/helper/actions.py
+++ src/tapper/helper/actions.py
@@ -13,6 +13,7 @@ def repeat_while(
     period_s: float = 0.1,
     max_repeats: int | None = None,
 ) -> Callable[[], None]:
+    """Repeats an action while condition applies."""
     max_repeats_int = max_repeats or 999999999999999
 
     def fn() -> None:
@@ -28,7 +29,19 @@ def repeat_while(
 def toggle_repeat(
     action: Callable[[], Any], period_s: float = 0.1, max_repeats: int | None = None
 ) -> Callable[[], None]:
-    """When toggled, starts repeating the action until toggled again."""
+    """
+    When toggled, starts repeating the action until toggled again.
+
+    Notes:
+        - runs in one separate thread, so cannot launch multiple toggle_repeats
+        - execution is immediately overridden by triggering another toggle_repeat
+        - execution immediately stops on second toggle
+
+    :param action: what to repeat
+    :param period_s: how often to repeat, seconds
+    :param max_repeats: optional limit
+    :return: callable toggle, to be set into a Tap
+    """
     max_repeats_int = max_repeats or 999999999999999
     repeater.registered_repeatables[action] = period_s, max_repeats_int
     return partial(_util.repeater.toggle_repeatable, action)
