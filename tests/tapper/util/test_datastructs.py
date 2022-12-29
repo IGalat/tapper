c3c3302c95ea1ba8e96261c4f238bf11664ad18f
Illia Halatiuk <galatyuk.ilya@gmail.com>
2022-12-28 20:04:57 -0500

    add some examples to flat list conversion

--- tests/tapper/util/test_datastructs.py
+++ tests/tapper/util/test_datastructs.py
@@ -10,7 +10,12 @@ from tapper.util import datastructs
 
 
 @given(strategies.primitives_and_seq)
-@hypothesis.settings(max_examples=20)
+@hypothesis.settings(max_examples=1)
+@hypothesis.example([1])
+@hypothesis.example("1")
+@hypothesis.example(("1",))
+@hypothesis.example({"1": "1"})
+@hypothesis.example([1, [2, [3, [4]]]])
 def test_to_flat_list(input_: Any | list[Any]) -> None:
     flat = datastructs.to_flat_list(input_)
     assert isinstance(flat, list)
