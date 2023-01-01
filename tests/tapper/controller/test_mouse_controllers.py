08a7a9cbcbc2457916e2d9026495ce312da53984
Illia Halatiuk <galatyuk.ilya@gmail.com>
2023-01-01 13:15:06 -0500

    fix test

--- tests/tapper/controller/test_mouse_controllers.py
+++ tests/tapper/controller/test_mouse_controllers.py
@@ -1,27 +1,20 @@
-import sys
+from functools import partial
 
-import pytest
-from tapper.model import constants
+from tapper.controller.mouse.mouse_api import calc_move
 
 
-@pytest.mark.skipif(sys.platform != constants.OS.win32, reason="")
-class TestWin32MouseController:
+class TestMouseController:
     def test_wheel_calc(self) -> None:
-        from tapper.controller.mouse.mouse_win32_winput_impl import (
-            Win32MouseTrackerCommander,
-        )
+        def get_pos() -> tuple[int, int]:
+            return 200, 200
 
-        mouse_tc = Win32MouseTrackerCommander()
+        c_move = partial(calc_move, get_pos)
 
-        current_x, current_y = 200, 200
+        assert c_move(500, 500, False) == (500, 500)
+        assert c_move(None, 100, False) == (200, 100)
+        assert c_move(100, None, False) == (100, 200)
 
-        mouse_tc.get_pos = lambda: (current_x, current_y)
-
-        assert mouse_tc.calc_move(500, 500, False) == (500, 500)
-        assert mouse_tc.calc_move(None, 100, False) == (200, 100)
-        assert mouse_tc.calc_move(100, None, False) == (100, 200)
-
-        assert mouse_tc.calc_move(10, 10, True) == (210, 210)
-        assert mouse_tc.calc_move(-20, -20, True) == (180, 180)
-        assert mouse_tc.calc_move(-20, None, True) == (180, 200)
-        assert mouse_tc.calc_move(None, -20, True) == (200, 180)
+        assert c_move(10, 10, True) == (210, 210)
+        assert c_move(-20, -20, True) == (180, 180)
+        assert c_move(-20, None, True) == (180, 200)
+        assert c_move(None, -20, True) == (200, 180)
