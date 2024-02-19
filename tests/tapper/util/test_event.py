339c8ad7df7b59a7f7ab93fbfd450102e7ce5be8
Illia Halatiuk <galatyuk.ilya@gmail.com>
2024-02-19 14:46:06 +0700

    Event - make possible to unsubscribe from subscribed function

--- tests/tapper/util/test_event.py
+++ tests/tapper/util/test_event.py
@@ -5,22 +5,45 @@ from tapper.util import event
 
 
 class Subscriber:
-    received_message: Any
+    received_messages: list[Any]
 
-    def receive_message(self, sub_message: Any) -> None:
-        self.received_message = sub_message
+    def __init__(self) -> None:
+        self.received_messages = list()
+
+    def receive_message(self, sub_message: Any) -> bool:
+        self.received_messages.append(sub_message)
+        return self.is_unsub(sub_message)
+
+    def is_unsub(self, message: Any) -> bool:
+        return True
 
 
-subscriber = Subscriber()
 topic_name = "test_topic"
 
 
 @pytest.mark.parametrize("message", [("a", True), 123, ("lmb", "DOWN")])
 def test_pubsub(message: Any) -> None:
-    event.publish("non-existing topic doesn't break anything", 123)
+    subscriber = Subscriber()
+    event.publish("non-existing topic doesn't break anything", 12345)
     event.subscribe(topic_name, subscriber.receive_message)
     event.publish(topic_name, message)
     event.unsubscribe(topic_name, subscriber.receive_message)
     event.publish(topic_name, "new message")
 
-    assert subscriber.received_message is message
+    assert subscriber.received_messages == [message]
+
+
+def test_auto_unsub() -> None:
+    def unsub(message: Any) -> bool:
+        return message != ("b", True)
+
+    subscriber = Subscriber()
+    subscriber.is_unsub = unsub
+
+    event.subscribe(topic_name, subscriber.receive_message)
+    [
+        event.publish(topic_name, m)
+        for m in [("a", True), ("a", False), ("b", True), ("b", False)]
+    ]
+
+    assert subscriber.received_messages == [("a", True), ("a", False), ("b", True)]
