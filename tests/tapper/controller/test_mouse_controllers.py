66cbdbd2cecac62010055b6697c5853c209e4d5f
Illia Halatiuk <galatyuk.ilya@gmail.com>
2024-09-30 23:08:06 +0700

    tests for new mouse api

--- tests/tapper/controller/test_mouse_controllers.py
+++ tests/tapper/controller/test_mouse_controllers.py
@@ -1,6 +1,10 @@
 from functools import partial
 
 from tapper.controller.mouse.mouse_api import calc_move
+from tapper.controller.mouse.mouse_api import MouseController
+from tapper.model import constants
+from tapper.state import keeper
+from testutil_model import Dummy
 
 
 class TestMouseController:
@@ -18,3 +22,45 @@ class TestMouseController:
         assert c_move(-20, -20, True) == (180, 180)
         assert c_move(-20, None, True) == (180, 200)
         assert c_move(None, -20, True) == (200, 180)
+
+    def test_tuple_move(self, dummy: Dummy) -> None:
+        mouse_tc = dummy.MouseTC(dummy.Listener.get_for_os(""), [])
+
+        mouse = MouseController()
+        mouse._tracker = mouse_tc
+        mouse._commander = mouse_tc
+        mouse.move((1, 2))
+        assert mouse_tc.x == 1
+        assert mouse_tc.y == 2
+
+    def test_click(self, dummy: Dummy) -> None:
+        mouse_tc = dummy.MouseTC(dummy.Listener.get_for_os(""), [])
+        mouse_tc.listener.on_signal = lambda x: True
+        mouse = MouseController()
+        mouse._tracker = mouse_tc
+        mouse._commander = mouse_tc
+        mouse._emul_keeper = keeper.Emul()
+
+        mouse.click(1, 2)
+        assert mouse_tc.x == 1
+        assert mouse_tc.y == 2
+        assert mouse_tc.all_signals == [
+            ("left_mouse_button", constants.KeyDirBool.DOWN),
+            ("left_mouse_button", constants.KeyDirBool.UP),
+        ]
+
+    def test_right_click(self, dummy: Dummy) -> None:
+        mouse_tc = dummy.MouseTC(dummy.Listener.get_for_os(""), [])
+        mouse_tc.listener.on_signal = lambda x: True
+        mouse = MouseController()
+        mouse._tracker = mouse_tc
+        mouse._commander = mouse_tc
+        mouse._emul_keeper = keeper.Emul()
+
+        mouse.right_click(1, 2)
+        assert mouse_tc.x == 1
+        assert mouse_tc.y == 2
+        assert mouse_tc.all_signals == [
+            ("right_mouse_button", constants.KeyDirBool.DOWN),
+            ("right_mouse_button", constants.KeyDirBool.UP),
+        ]
